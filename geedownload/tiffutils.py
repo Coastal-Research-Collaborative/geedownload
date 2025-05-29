3c0ddc5441db466ab529f94cdf554037cd3947d8
Joel Nicolow <jnicolow@hawaii.edu>
2025-05-29 12:47:34 -1000

    Update tiffutils.py

--- geedownload/tiffutils.py
+++ geedownload/tiffutils.py
@@ -639,6 +639,7 @@ def clean_up_gee_downloads(data_dir):
         for timestamp in timestamps:
             # print(f'{satname}*{timestamp}*.*.tif')
             glob_pattern = os.path.join(sat_data_dir, f'{satname}*{timestamp}*.*.tif') # this is general so it works for sentinel and landsat
+            print(os.path.join(sat_data_dir, f'{satname}*{timestamp}*.*.tif'))
             fns = glob(glob_pattern)
             # if len(fns) == 0:  # NOTE this should only happen if there are some weirdly named files in the folder
             #     print(f'somehow there are no images for {timestamp=}')
